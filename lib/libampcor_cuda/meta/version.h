a7ee45cf9ae9da15b7f71e5dda89034450663567
Michael Aïvázis <michael.aivazis@para-sim.com>
2018-12-09 16:35:53 -0800

    libamcpor_cuda: added the skeleton

---
+++ lib/libampcor_cuda/meta/version.h
@@ -0,0 +1,27 @@
+// -*- C++ -*-
+// -*- coding: utf-8 -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// parasim
+// (c) 1998-2018 all rights reserved
+//
+
+
+// code guard
+#if !defined(ampcor_libampcor_cuda_meta_version_h)
+#define ampcor_libampcor_cuda_meta_version_h
+
+// my declarations
+namespace ampcor {
+    namespace cuda {
+        namespace meta {
+            // the version number of the {ampcor} library
+            extern const char * version();
+        } // of namespace meta
+    } // of namespace cuda
+} // of namespace ampcor
+
+#endif
+
+
+// end of file
