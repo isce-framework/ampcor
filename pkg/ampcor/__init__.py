e6160c71246d12f268bd03aa5ef6079abdc0b2ce
Michael Aïvázis <michael.aivazis@para-sim.com>
2018-12-09 16:38:37 -0800

    ampcor: added the package skeleton

---
+++ pkg/ampcor/__init__.py
@@ -0,0 +1,73 @@
+# -*- Python -*-
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# parasim
+# (c) 1998-2018 all rights reserved
+#
+
+
+# import and publish pyre symbols
+from pyre import (
+    # protocols, components, traits, and their infrastructure
+    schemata, constraints, properties, protocol, component, foundry,
+    # decorators
+    export, provides,
+    # the manager of the pyre runtime
+    executive,
+    # support for concurrency
+    nexus,
+    # support for multidimensional containers
+    grid,
+    # miscellaneous
+    tracking, units
+    )
+
+
+# bootstrap
+package = executive.registerPackage(name='ampcor', file=__file__)
+# save the geography
+home, prefix, defaults = package.layout()
+
+
+# administrivia
+def copyright():
+    """
+    Return the copyright note
+    """
+    # pull and print the meta-data
+    return print(meta.header)
+
+
+def license():
+    """
+    Print the license
+    """
+    # pull and print the meta-data
+    return print(meta.license)
+
+
+def built():
+    """
+    Return the build timestamp
+    """
+    # pull and return the meta-data
+    return meta.date
+
+
+def credits():
+    """
+    Print the acknowledgments
+    """
+    return print(meta.acknowledgments)
+
+
+def version():
+    """
+    Return the version
+    """
+    # pull and return the meta-data
+    return meta.version
+
+
+# end of file
---
+++ pkg/ampcor/exceptions.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis
+# orthologue
+# (c) 2003-2018 all rights reserved
+#
+
+
+# get the framework
+import pyre
+
+# the base class for my exceptions
+class AmpcorError(pyre.PyreError):
+    """
+    Base class for all ampcor errors
+    """
+
+# component configuration errors
+class ConfigurationError(AmpcorError):
+    """
+    Exception raised when ampcor components detect inconsistencies in their configurations
+    """
+
+    # public data
+    description = "configuration error: {0.reason}"
+
+    # meta-methods
+    def __init__(self, reason, **kwds):
+        # chain up
+        super().__init__(**kwds)
+        # save the error info
+        self.reason = reason
+        # all done
+        return
+
+
+# end of file
