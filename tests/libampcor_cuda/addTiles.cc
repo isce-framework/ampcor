6d39e4ac833f141c5bea33f8bfc9b7555fd65518
Michael Aïvázis <michael.aivazis@para-sim.com>
2019-03-10 09:55:43 -0700

    libampcor_cuda: the {addTiles} test case now verifies that the reference data was placed correctly in the correlator's arena

--- tests/libampcor_cuda/addTiles.cc
+++ tests/libampcor_cuda/addTiles.cc
@@ -18,8 +18,12 @@
 #include <ampcor_cuda/correlators.h>
 
 // type aliases
+// my value type
+using value_t = float;
+// the pixel type
+using pixel_t = std::complex<value_t>;
 // my raster type
-using slc_t = pyre::grid::simple_t<2, std::complex<float>>;
+using slc_t = pyre::grid::simple_t<2, pixel_t>;
 // the correlator
 using correlator_t = ampcor::cuda::correlators::sequential_t<slc_t>;
 
@@ -32,22 +36,29 @@ int main() {
 
     // make a channel for logging progress
     pyre::journal::debug_t channel("ampcor.cuda");
-    // show me
+    // sign in
     channel
         << pyre::journal::at(__HERE__)
-        << "setting up the correlation plan with the cuda ampcor task manager"
+        << "test: adding reference and target tile pairs to the correlator"
         << pyre::journal::endl;
 
     // the reference tile extent
-    int refExt = 4;
+    int refExt = 128;
     // the margin around the reference tile
-    int margin = 2;
+    int margin = 32;
     // therefore, the target tile extent
     int tgtExt = refExt + 2*margin;
     // the number of possible placements of the reference tile within the target tile
     int placements = 2*margin + 1;
     // the number of pairs
     slc_t::size_type pairs = placements*placements;
+
+    // the number of cells in a reference tile
+    slc_t::size_type refCells = refExt * refExt;
+    // the number of cells in a target tile
+    slc_t::size_type tgtCells = tgtExt * tgtExt;
+    // the number of cells per pair
+    slc_t::size_type cellsPerPair = refCells + tgtCells;
 
     // the reference shape
     slc_t::shape_type refShape = {refExt, refExt};
@@ -68,7 +79,7 @@ int main() {
     // show me
     tlog
         << pyre::journal::at(__HERE__)
-        << "instantiating the manager: " << 1e6 * timer.read() << " μs"
+        << "instantiating the manager: " << 1e3 * timer.read() << " ms"
         << pyre::journal::endl;
 
     // start the clock
@@ -115,7 +126,54 @@ int main() {
     // show me
     tlog
         << pyre::journal::at(__HERE__)
-        << "creating reference dataset: " << 1e6 * timer.read() << " μs"
+        << "creating reference dataset: " << 1e3 * timer.read() << " ms"
+        << pyre::journal::endl;
+
+    // verify
+    // start the clock
+    timer.reset().start();
+    // get the arena
+    auto arena = c.arena();
+    // go through all pairs
+    for (auto i=0; i<placements; ++i) {
+        for (auto j=0; i<placements; ++i) {
+            // compute the pair id
+            auto pid = i*placements + j;
+            // get the reference raster
+            auto ref = arena + pid*cellsPerPair;
+            // verify its contents
+            for (auto idx=0; idx<refExt; ++idx) {
+                for (auto jdx=0; jdx<refExt; ++jdx) {
+                    // the expected value
+                    pixel_t expected = pid;
+                    // the actual value
+                    pixel_t actual = ref[idx*refExt + jdx];
+                    // compute the mismatch
+                    auto mismatch = std::abs(expected-actual)/std::abs(expected);
+                    // if there is a mismatch
+                    if (mismatch > std::numeric_limits<float>::epsilon()) {
+                        // make a channel
+                        pyre::journal::error_t error("ampcor.cuda");
+                        // complain
+                        error
+                            << pyre::journal::at(__HERE__)
+                            << "ref[" << pid << "; " << idx << ", " << jdx << "] : mismatch: "
+                            << "expected: " << expected
+                            << ", actual: " << actual
+                            << pyre::journal::endl;
+                        // and bail
+                        throw std::runtime_error("verification error");
+                    }
+                }
+            }
+        }
+    }
+    // stop the clock
+    timer.stop();
+    // show me
+    tlog
+        << pyre::journal::at(__HERE__)
+        << "verifying reference dataset: " << 1e3 * timer.read() << " ms"
         << pyre::journal::endl;
 
     // show me
