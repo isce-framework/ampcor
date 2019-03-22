bb33391723fc79816e69e0ee6c1840a8564bff8b
Michael Aïvázis <michael.aivazis@para-sim.com>
2019-03-22 09:20:03 -0700

    libampcor_cuda: added test cases to exercise reference and target tile refinement

--- tests/libampcor_cuda/refine.cc
+++ tests/libampcor_cuda/refine.cc
@@ -31,20 +31,22 @@ using slc_t = pyre::grid::simple_t<2, pixel_t>;
 // the correlator
 using correlator_t = ampcor::cuda::correlators::sequential_t<slc_t>;
 
+// adapt a chunk of memory into a tile
+using mem_t = pyre::memory::constview_t<slc_t::cell_type>;
+using tile_t = pyre::grid::grid_t<slc_t::cell_type, slc_t::layout_type, mem_t>;
+
 // driver
 int main() {
-    // pick a device
-    cudaSetDevice(5);
     // number of gigabytes per byte
     const auto Gb = 1.0/(1024*1024*1024);
 
     // make a timer
     pyre::timer_t timer("ampcor.cuda.sanity");
     // make a channel for reporting the timings
-    pyre::journal::info_t tlog("ampcor.cuda.tlog");
+    pyre::journal::debug_t tlog("ampcor.cuda.tlog");
 
     // make a channel for logging progress
-    pyre::journal::info_t channel("ampcor.cuda");
+    pyre::journal::debug_t channel("ampcor.cuda");
     // show me
     channel
         << pyre::journal::at(__HERE__)
@@ -52,13 +54,13 @@ int main() {
         << pyre::journal::endl;
 
     // the reference tile extent
-    int refExt = 32;
+    int refExt = 128;
     // the margin around the reference tile
-    int margin = 1;
+    int margin = 32;
     // the refinement factor
     int refineFactor = 2;
     // the margin around the refined target tile
-    int refineMargin = 4;
+    int refineMargin = 8;
     // therefore, the target tile extent
     auto tgtExt = refExt + 2*margin;
     // the number of possible placements of the reference tile within the target tile
@@ -79,11 +81,27 @@ int main() {
     slc_t::shape_type refShape = {refExt, refExt};
     // the search window shape
     slc_t::shape_type tgtShape = {tgtExt, tgtExt};
-
     // the reference layout with the given shape and default packing
     slc_t::layout_type refLayout = { refShape };
     // the search window layout with the given shape and default packing
     slc_t::layout_type tgtLayout = { tgtShape };
+
+    // the shape of the refined reference tiles
+    auto refRefinedShape = refineFactor * refShape;
+    // the shape of the refined target tiles
+    auto tgtRefinedShape = refineFactor * (refShape + slc_t::index_type::fill(2*refineMargin));
+    // the layout of the refined reference tiles
+    slc_t::layout_type refRefinedLayout { refRefinedShape };
+    // the layout of the refined target tiles
+    slc_t::layout_type tgtRefinedLayout { tgtRefinedShape };
+    // the number of cells in a refined reference tile
+    auto refRefinedCells = refRefinedLayout.size();
+    // the number of cells in a refined target tile
+    auto tgtRefinedCells = tgtRefinedLayout.size();
+    //  the number of cells per refined pair
+    auto cellsPerRefinedPair = refRefinedCells + tgtRefinedCells;
+    // the total number of refined cells
+    auto cellsRefined = pairs * cellsPerRefinedPair;
 
     // start the clock
     timer.reset().start();
@@ -189,7 +207,273 @@ int main() {
         << "refining the reference tiles: " << 1e3 * timer.read() << " ms"
         << pyre::journal::endl;
 
+    // let's bring the refined arena back to the host
+    // the memory footprint
+    auto refinedFootprint = cellsRefined * sizeof(slc_t::cell_type);
+    // make room
+    auto refined = new slc_t::cell_type[cellsRefined];
+    // start the clock
+    timer.reset().start();
+    // copy the results over
+    auto status = cudaMemcpy(refined, refinedArena, refinedFootprint, cudaMemcpyDeviceToHost);
+    // stop the clock
+    timer.stop();
+    // if something went wrong
+    if (status != cudaSuccess) {
+        // get the error description
+        std::string description = cudaGetErrorName(status);
+        // make a channel
+        pyre::journal::error_t error("ampcor.cuda");
+        // complain
+        error
+            << pyre::journal::at(__HERE__)
+            << "while retrieving the refined tiles: "
+            << description << " (" << status << ")"
+            << pyre::journal::endl;
+        // and bail
+        throw std::runtime_error(description);
+    }
+    // get the duration
+    duration = timer.read();
+    // compute the transfer rate
+    rate = refinedFootprint / duration * Gb;
+    // show me
+    tlog
+        << pyre::journal::at(__HERE__)
+        << "moving the refined tiles to the host: " << 1e3 * duration << " ms"
+        << ", at " << rate << " Gb/s"
+        << pyre::journal::endl;
+
+    // start the clock
+    timer.reset().start();
+    // establish a tolerance
+    auto tolerance = std::numeric_limits<value_t>::epsilon();
+    // verify that all the cells that would have been occupied by the target tiles are still zero
+    for (auto pid=0; pid < pairs; ++pid) {
+        // find the starting point of this target tile
+        auto tgtTile = refined + pid*cellsPerRefinedPair + refRefinedCells;
+        // make a tile over it
+        tile_t tgt { tgtRefinedShape, tgtTile };
+        // go through all the cells
+        for (auto idx : tgt.layout()) {
+            // get the value
+            auto cell = tgt[idx];
+            // check that it is zero
+            if (std::abs(cell) > tolerance) {
+                // make a channel
+                pyre::journal::error_t error("ampcor.cuda");
+                // show me
+                error
+                    << pyre::journal::at(__HERE__)
+                    << "non-zero target cell: " << cell
+                    << pyre::journal::newline
+                    << "at [" << pid << ";" << idx << "]"
+                    << pyre::journal::endl;
+                // and bail
+                throw std::runtime_error("verification error");
+            }
+        }
+    }
+    // stop the clock
+    timer.stop();
+    // show me
+    tlog
+        << pyre::journal::at(__HERE__)
+        << "verifying that the refined target slots were untouched: " << 1e3 * duration << " ms"
+        << pyre::journal::endl;
+
+    // let's start over with the target refinement check
+    // get rid of the refined arena
+    cudaFree(refinedArena);
+    // start the clock
+    timer.reset().start();
+    // allocate a new one
+    refinedArena = c._refinedArena();
+    // stop the clock
+    timer.stop();
+    // show me
+    tlog
+        << pyre::journal::at(__HERE__)
+        << "reallocating the refined tile arena: " << 1e3 * timer.read() << " ms"
+        << pyre::journal::endl;
+
+    // synthesize the locations of the maxima
+    // start the clock
+    timer.reset().start();
+    // a (row, col) index for each pair
+    int * maxloc = new int[2*pairs];
+    // initialize it
+    for (auto pid=0; pid < pairs; ++pid) {
+        // decode the row and column and place the values
+        maxloc[2*pid] = pid / placements;
+        maxloc[2*pid + 1] = pid % placements;
+    }
+    // stop the clock
+    timer.stop();
+    // show me
+    tlog
+        << pyre::journal::at(__HERE__)
+        << "synthesizing locations for the refined target tiles: " << 1e3 * timer.read() << " ms"
+        << pyre::journal::endl;
+
+    // start the clock
+    timer.reset().start();
+    // on the device
+    int * dloc;
+    // we need
+    auto locFootprint = 2 * pairs * sizeof(int);
+    // make some room
+    status = cudaMallocManaged(&dloc, locFootprint);
+    // if something went wrong
+    if (status != cudaSuccess) {
+        // make a channel
+        pyre::journal::error_t error("ampcor.cuda");
+        // complain
+        error
+            << pyre::journal::at(__HERE__)
+            << "while allocating memory for the locations of the correlation maxima: "
+            << cudaGetErrorName(status) << " (" << status << ")"
+            << pyre::journal::endl;
+        // bail
+        throw std::bad_alloc();
+    }
+    // move the locations to the device
+    status = cudaMemcpy(dloc, maxloc, locFootprint, cudaMemcpyHostToDevice);
+    // if something went wrong
+    if (status != cudaSuccess) {
+        // get the error description
+        std::string description = cudaGetErrorName(status);
+        // make a channel
+        pyre::journal::error_t error("ampcor.cuda");
+        // complain
+        error
+            << pyre::journal::at(__HERE__)
+            << "while moving the locations of the correlation maxima to the device: "
+            << description << " (" << status << ")"
+            << pyre::journal::endl;
+        // bail
+        throw std::runtime_error(description);
+    }
+    // stop the clock
+    timer.stop();
+    // show me
+    tlog
+        << pyre::journal::at(__HERE__)
+        << "moving maxima locations to the device: " << 1e3 * timer.read() << " ms"
+        << pyre::journal::endl;
+
+    // start the clock
+    timer.reset().start();
+    // nudge them
+    c._nudge(dloc);
+    // stop the clock
+    timer.stop();
+    // show me
+    tlog
+        << pyre::journal::at(__HERE__)
+        << "nudging locations: " << 1e3 * timer.read() << " ms"
+        << pyre::journal::endl;
+
+    // start the clock
+    timer.reset().start();
+    // migrate
+    c._tgtMigrate(coarseArena, dloc, refinedArena);
+    // stop the clock
+    timer.stop();
+    // show me
+    tlog
+        << pyre::journal::at(__HERE__)
+        << "migrating the expanded maxcor tiles to the refinement arena "
+        << 1e3 * timer.read() << " ms"
+        << pyre::journal::endl;
+
+
+    // start the clock
+    timer.reset().start();
+    // refine the target tiles
+    c._tgtRefine(refinedArena);
+    // stop the clock
+    timer.stop();
+    // show me
+    tlog
+        << pyre::journal::at(__HERE__)
+        << "refining the target tiles: " << 1e3 * timer.read() << " ms"
+        << pyre::journal::endl;
+
+    // let's bring the refined arena back to the host
+    // start the clock
+    timer.reset().start();
+    // copy the results over
+    status = cudaMemcpy(refined, refinedArena, refinedFootprint, cudaMemcpyDeviceToHost);
+    // stop the clock
+    timer.stop();
+    // if something went wrong
+    if (status != cudaSuccess) {
+        // get the error description
+        std::string description = cudaGetErrorName(status);
+        // make a channel
+        pyre::journal::error_t error("ampcor.cuda");
+        // complain
+        error
+            << pyre::journal::at(__HERE__)
+            << "while retrieving the refined tiles: "
+            << description << " (" << status << ")"
+            << pyre::journal::endl;
+        // and bail
+        throw std::runtime_error(description);
+    }
+    // get the duration
+    duration = timer.read();
+    // compute the transfer rate
+    rate = refinedFootprint / duration * Gb;
+    // show me
+    tlog
+        << pyre::journal::at(__HERE__)
+        << "moving the second set of refined tiles to the host: " << 1e3 * duration << " ms"
+        << ", at " << rate << " Gb/s"
+        << pyre::journal::endl;
+
+    // start the clock
+    timer.reset().start();
+    // verify that all the cells that would have been occupied by the reference tiles are still zero
+    for (auto pid=0; pid < pairs; ++pid) {
+        // find the starting point of this target tile
+        auto refTile = refined + pid*cellsPerRefinedPair;
+        // make a tile over it
+        tile_t ref { refRefinedShape, refTile };
+        // go through all the cells
+        for (auto idx : ref.layout()) {
+            // get the value
+            auto cell = ref[idx];
+            // check that it is zero
+            if (std::abs(cell) > tolerance) {
+                // make a channel
+                pyre::journal::error_t error("ampcor.cuda");
+                // show me
+                error
+                    << pyre::journal::at(__HERE__)
+                    << "non-zero reference cell: " << cell
+                    << pyre::journal::newline
+                    << "at [" << pid << ";" << idx << "]"
+                    << pyre::journal::endl;
+                // and bail
+                throw std::runtime_error("verification error");
+            }
+        }
+    }
+    // stop the clock
+    timer.stop();
+    // show me
+    tlog
+        << pyre::journal::at(__HERE__)
+        << "verifying that the refined reference slots were untouched: "
+        << 1e3 * timer.read() << " ms"
+        << pyre::journal::endl;
+
     // clean up
+    delete [] maxloc;
+    delete [] refined;
+    cudaFree(dloc);
     cudaFree(refinedArena);
     cudaFree(coarseArena);
 
